2751e3d0edcc1e7b17880c71a48dde9a3d2cc3d3
GlebTutaev <aka-laka-103@mail.ru>
2023-01-30 22:03:04 +0500

    Practice17(mbFinish)

--- app/src/main/java/com/example/project16/LoadedUserDataActivity.java
+++ app/src/main/java/com/example/project16/LoadedUserDataActivity.java
@@ -2,18 +2,60 @@ package com.example.project16;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 public class LoadedUserDataActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loaded_user_data);
+        Init();
 
     }
 
+    private void Init() {
+        mediaPlayer = MediaPlayer.create(this,R.raw.sound);
+        mediaPlayer.setVolume(1F, 1F);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mediaPlayer!=null){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
 }
